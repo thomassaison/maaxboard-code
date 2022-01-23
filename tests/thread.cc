17cf08a0dbcce99edb37f3187685ca26205757e0
leo.cardao <leo.cardao@epita.fr>
2022-01-23 19:11:01 +0100

    add test

--- tests/thread.cc
+++ tests/thread.cc
@@ -1,10 +1,9 @@
 #include <gtest/gtest.h>
 
-#include "imx8m/config/config.hh"
+#include "imx8m/thread/thread.hh"
 
 using namespace imx8m::thread;
 
-//architecture example from : https://github.com/google/googletest/blob/main/googletest/samples/sample4_unittest.cc
 namespace {
     TEST(Thread, constructors) {
         Thread t1;
@@ -12,5 +11,26 @@ namespace {
 
         Thread t2(std::move(t1));
         EXPECT_FALSE(t2.in_work());
+    }
+
+    TEST(Thread, operator_eq) {
+        Thread t1, t2;
+        EXPECT_FALSE(t1.in_work());
+        EXPECT_FALSE(t2.in_work());
+
+        t1 = std::move(t2);
+    }
+
+    TEST(Thread, set_sched) {
+        Thread t;
+        t.set_sched(SCHED_FIFO, 10);
+    }
+
+    TEST(Thread, queue) {
+        Thread t;
+        for (size_t x = 0; x < 10; ++x)
+            t.queue([&](int i){ std::cerr << "From thread " << i << std::endl; }, x);
+        sleep(1);
+        std::cout << "From Main thread" << std::endl;
     }
 }
